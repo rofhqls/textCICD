a233d7802b6b5d7706cfc33365e5a6412d9a346a
rofhqls <rovin1025@naver.com>
2026-09-04 10:00:10 +0900

    first commit

---
+++ src/main/java/com/fastcam/cicd00/Cicd00Application.java
@@ -0,0 +1,13 @@
+package com.fastcam.cicd00;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cicd00Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cicd00Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/fastcam/cicd00/Cicd00ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcam.cicd00;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicd00ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
